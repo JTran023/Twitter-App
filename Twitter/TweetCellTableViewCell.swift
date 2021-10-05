2897bbe7fa0bd0024c289dd1e83c52f04a2475e3
JTran013 <jtran023@fiu.edu>
2021-10-04 22:35:55 -0400

    setFavorite()

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -13,9 +13,21 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
-    
     @IBOutlet weak var retweetButton: UIButton!
     @IBOutlet weak var likeButton: UIButton!
+    
+    var favorited:Bool = false
+    
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        if(isFavorited){
+            likeButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        }
+        else{
+            likeButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
     
     @IBAction func retweet(_ sender: Any) {
     }
